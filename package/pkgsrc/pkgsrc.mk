73262ba637476b9d5a460d71ee78dd59936a27b5
Robert Nagy <robert@cradleware.io>
2015-03-17 17:29:39 +0100

    bump release to 2015.6

--- package/pkgsrc/pkgsrc.mk
+++ package/pkgsrc/pkgsrc.mk
@@ -16,10 +16,6 @@ PKGSRC_EXTRACT_CMDS =
 define PKGSRC_INSTALL_TARGET_CMDS
 	$(INSTALL) -d -m 0755 $(TARGET_DIR)/var/db
 	cp -a $(DL_DIR)/$(PKGSRC_SOURCE) $(TARGET_DIR)/var/db
-
-	# Install some symlinks for mail
-	cd $(TARGET_DIR)/usr/sbin; ln -sf /usr/pkg/sbin/smtpctl sendmail
-	cd $(TARGET_DIR)/var; ln -sf /usr/pkg/var/spool/mail mail
 endef
 
 $(eval $(generic-package))
