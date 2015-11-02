1168e26d001287a6ae4ed1d8657e0aab0caec53a
Robert Nagy <robert@redcoolbeans.com>
2015-11-02 16:02:18 +0100

    add support for tar ops.

--- fs/docker/docker.mk
+++ fs/docker/docker.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 define ROOTFS_DOCKER_CMD
-	tar --exclude="*usr/include*" --numeric-owner -C $(TARGET_DIR) -c . | docker import - $(BR2_TARGET_ROOTFS_DOCKER_NAME)
+	tar $(BR2_TARGET_ROOTFS_DOCKER_TAR_OPTS) --numeric-owner -C $(TARGET_DIR) -c . | docker import - $(BR2_TARGET_ROOTFS_DOCKER_NAME)
 endef
 
 $(eval $(call ROOTFS_TARGET,docker))
