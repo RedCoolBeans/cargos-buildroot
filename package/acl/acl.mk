1821680839f18590a7e6770636076dfa3043d820
Robert Nagy <robert@redcoolbeans.com>
2017-03-27 10:23:37 +0200

    acl: make sure build picks up TARGET_CFLAGS
    The acl build system doesn't use automake, therefore it is broken. It
    doesn't use the CFLAGS passed by configure. Work around this by passing
    CFLAGS in the environment. The makefiles append to CFLAGS, so this
    works.
    
    This issue hasn't led to build failures, but it is visible e.g. when
    stack protector is enabled: the stack protector options are not applied
    to acl. Also debug and optimisation options aren't applied.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
    Cc: Yegor Yefremov <yegorslists@googlemail.com>
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
    
    Conflicts:
    	package/acl/acl.mk

--- package/acl/acl.mk
+++ package/acl/acl.mk
@@ -15,11 +15,17 @@ ACL_LICENSE_FILES = doc/COPYING doc/COPYING.LGPL
 
 # While the configuration system uses autoconf, the Makefiles are
 # hand-written and do not use automake. Therefore, we have to hack
-# around their deficiencies by passing installation paths.
-ACL_INSTALL_STAGING_OPTS = 			\
-	prefix=$(STAGING_DIR)/usr 		\
-	exec_prefix=$(STAGING_DIR)/usr 		\
-	PKG_DEVLIB_DIR=$(STAGING_DIR)/usr/lib	\
+# around their deficiencies by:
+# - explicitly passing CFLAGS (LDFLAGS are passed on from configure,
+#   CFLAGS are not).
+# - explicitly passing the installation prefix, not using DESTDIR.
+
+ACL_MAKE_ENV = CFLAGS="$(TARGET_CFLAGS)"
+
+ACL_INSTALL_STAGING_OPTS = \
+	prefix=$(STAGING_DIR)/usr \
+	exec_prefix=$(STAGING_DIR)/usr \
+	PKG_DEVLIB_DIR=$(STAGING_DIR)/usr/lib \
 	install-dev install-lib
 
 ACL_INSTALL_TARGET_OPTS = 			\
